0309c2d7d7e9f580f7cf563eecd9557d55daed0d
Jefferson Licet <jeffersonlicet@gmail.com>
2017-12-16 20:38:40 -0400

    Adding take picture fun

--- app/src/main/java/net/sparkly/projectx/views/CameraActivity.java
+++ app/src/main/java/net/sparkly/projectx/views/CameraActivity.java
@@ -1,5 +1,7 @@
 package net.sparkly.projectx.views;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.res.AssetManager;
@@ -9,6 +11,8 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
@@ -20,6 +24,7 @@ import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.yarolegovich.discretescrollview.DiscreteScrollView;
@@ -29,6 +34,7 @@ import com.yarolegovich.discretescrollview.transform.ScaleTransformer;
 import net.sparkly.projectx.R;
 import net.sparkly.projectx.adapters.ModeListAdapter;
 import net.sparkly.projectx.models.SingleModeItem;
+import net.sparkly.projectx.utils.StorageManager;
 import net.sparkly.projectx.views.widgets.CameraSurfaceView;
 import net.sparkly.projectx.views.widgets.CameraView;
 import net.sparkly.projectx.views.widgets.FocusMarkerLayout;
@@ -42,6 +48,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -53,13 +60,22 @@ import butterknife.OnClick;
 
 public class CameraActivity extends AppCompatActivity
 {
-    private static final int BACK_CAMERA     = 0;
-    private static final int FRONTAL_CAMERA  = 1;
+    private static final String TAG = CameraActivity.class.getSimpleName();
+
+    private static final int BACK_CAMERA = 0;
+    private static final int FRONTAL_CAMERA = 1;
+    private static final int FLASH_OFF = 0;
+    private static final int FLASH_ON = 1;
+    private static final int FLASH_AUTO = 2;
+
+    private int actualFlash;
     private int actualCamera;
-    private static final String TAG          = CameraActivity.class.getSimpleName();
 
     private boolean canTakePicture;
     private boolean isFilteringEnabled;
+    private boolean pendingToggleFlash;
+    private boolean pendingToggleCamera;
+    private boolean pendingTakePicture;
 
     private ModeListAdapter adapter;
     private List<SingleModeItem> modes = new ArrayList<>();
@@ -68,6 +84,7 @@ public class CameraActivity extends AppCompatActivity
     private Activity mActivity;
     private GestureDetectorCompat mDetector;
     private FocusMarkerLayout focusMarkerLayout;
+    private StorageManager storageManager;
 
     @BindView(R.id.surfaceView)
     CameraView surfaceView;
@@ -84,21 +101,124 @@ public class CameraActivity extends AppCompatActivity
     @BindView(R.id.toggleCamera)
     ImageButton toggleCameraIcon;
 
-    @OnClick({R.id.toggleFlash, R.id.toggleCamera})
+    @BindView(R.id.cameraShutter)
+    RelativeLayout cameraShutter;
+
+    @BindView(R.id.photoThumbnail)
+    ImageView photoThumbnail;
+
+    @OnClick({R.id.toggleFlash, R.id.toggleCamera, R.id.cameraShutter})
     public void clickManager(View view)
     {
-        switch(view.getId())
+        switch (view.getId())
         {
             case R.id.toggleFlash:
+                toggleFlash();
                 break;
             case R.id.toggleCamera:
                 toggleCamera();
                 break;
+
+            case R.id.cameraShutter:
+                takePicture();
+                break;
         }
+    }
+
+    private void takePicture()
+    {
+        if(!canTakePicture) return;
+
+        pendingTakePicture = true;
+
+        new Thread(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                surfaceView.takeShot(new CameraSurfaceView.TakePictureCallback()
+                {
+                    @Override
+                    public void takePictureOK(final Bitmap bmp)
+                    {
+                        mActivity.runOnUiThread(new Runnable()
+                        {
+                            @Override
+                            public void run()
+                            {
+                                RoundedBitmapDrawable roundedBitmapDrawable= RoundedBitmapDrawableFactory.create(getResources(), bmp);
+                                roundedBitmapDrawable.setCircular(true);
+                                roundedBitmapDrawable.setAntiAlias(true);
+                                photoThumbnail.setImageDrawable(roundedBitmapDrawable);
+                                animateThumbnail();
+                                Log.d(TAG, bmp.getHeight() + " height and " + bmp.getWidth() + " width");
+                            }
+                        });
+
+                    }
+                }, true);
+
+                surfaceView.takePicture(new CameraSurfaceView.TakePictureCallback()
+                {
+                    @Override
+                    public void takePictureOK(Bitmap bmp)
+                    {
+                        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                        String name =  File.separator + "IMG_" + timeStamp + ".jpg";
+                        storageManager.createFile(name, bmp);
+                        pendingTakePicture = false;
+                    }
+
+                }, null, null, 1, false);
+            }
+        }).start();
+    }
+
+    private void toggleFlash()
+    {
+        if (pendingToggleFlash)
+            return;
+
+        pendingToggleFlash = true;
+        int flashBackup = actualFlash;
+        int drawable = 0;
+
+        switch (actualFlash)
+        {
+            case FLASH_OFF:
+                actualFlash = FLASH_ON;
+                drawable = R.drawable.ic_flash_on_white_36dp;
+                break;
+
+            case FLASH_ON:
+                actualFlash = FLASH_AUTO;
+                drawable = R.drawable.ic_flash_auto_white_36dp;
+                break;
+            case FLASH_AUTO:
+                actualFlash = FLASH_OFF;
+                drawable = R.drawable.ic_flash_off_white_36dp;
+                break;
+        }
+
+        boolean response = surfaceView.setFlashLightMode(actualFlash);
+
+        if (response)
+            toggleFlashIcon.setImageDrawable(getResources().getDrawable(drawable));
+
+
+        else
+            actualFlash = flashBackup;
+
+        pendingToggleFlash = false;
     }
 
     private void toggleCamera()
     {
+        if (pendingToggleCamera)
+            return;
+
+        pendingToggleCamera = true;
+
         try
         {
             actualCamera = actualCamera == BACK_CAMERA ? FRONTAL_CAMERA : BACK_CAMERA;
@@ -109,11 +229,12 @@ public class CameraActivity extends AppCompatActivity
             surfaceView.setCameraFacing(actualCamera);
             surfaceView.onPause();
             surfaceView.onResume();
-        }
-        catch (Exception ex)
+        } catch (Exception ex)
         {
             ex.printStackTrace();
         }
+
+        pendingToggleCamera = false;
     }
 
     public CGENativeLibrary.LoadImageCallback mLoadImageCallback = new CGENativeLibrary.LoadImageCallback()
@@ -168,6 +289,14 @@ public class CameraActivity extends AppCompatActivity
         relativeWrapper.setClickable(false);
 
         mActivity = this;
+
+        try {
+            storageManager = new StorageManager(this);
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
 
         try
         {
@@ -272,13 +401,38 @@ public class CameraActivity extends AppCompatActivity
                                 }
                             }
                         }, null, "@curve G(0, 35)(255, 255)B(0, 133)(255, 255) @adjust brightness 0.03 @adjust contrast 1.35 @curve G(0, 13)(255, 255)B(88, 0)(255, 255) @adjust brightness -0.04 @adjust contrast 0.8 @pixblend multiply 250 223 182 255 100", 0.5f, false);
-                    */}
+                    */
+                    }
                 }
             }
         });
 
         //surfaceView.requestPortrait();
 
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        CameraInstance.getInstance().stopCamera();
+        surfaceView.release(null);
+        surfaceView.onPause();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        surfaceView.onResume();
+        modeSelector.post(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                modeSelector.smoothScrollToPosition(1);
+            }
+        });
     }
 
     private void performSwipeRight()
@@ -327,35 +481,26 @@ public class CameraActivity extends AppCompatActivity
         });
     }
 
-    @Override
-    public void onPause()
+    private void animateThumbnail()
     {
-        super.onPause();
-        CameraInstance.getInstance().stopCamera();
-        surfaceView.release(null);
-        surfaceView.onPause();
-    }
-
-    @Override
-    public void onResume()
-    {
-        super.onResume();
-        surfaceView.onResume();
-
-        modeSelector.post(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                modeSelector.smoothScrollToPosition(1);
-            }
-        });
+        photoThumbnail.animate().setListener(null).cancel();
+        photoThumbnail.setScaleX(2f);
+        photoThumbnail.setScaleY(2f);
+        photoThumbnail.setAlpha(0.3f);
+        photoThumbnail.animate().alpha(1f).scaleX(1).scaleY(1).setStartDelay(0).setDuration(330)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                    }
+                }).start();
 
     }
 
     @SuppressLint("ClickableViewAccessibility")
     private void buildCamera()
     {
+        surfaceView.setStorageManager(storageManager);
         surfaceView.setCameraFacing(BACK_CAMERA);
 
         surfaceView.setOnCreateCallback(new CameraView.OnCreateCallback()
@@ -364,19 +509,9 @@ public class CameraActivity extends AppCompatActivity
             public void createOver(boolean success)
             {
                 isFilteringEnabled = true;
+                canTakePicture = true;
             }
         });
-
-
-        /*surfaceView.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View arg0)
-            {
-                //Toast.makeText(getApplicationContext(), " simple touch", Toast.LENGTH_SHORT).show();
-
-            }
-        });*/
 
         surfaceView.setOnTouchListener(new View.OnTouchListener()
         {
@@ -387,9 +522,7 @@ public class CameraActivity extends AppCompatActivity
                 return true;
             }
         });
-
     }
-
 
     private class CameraGestureListener extends GestureDetector.SimpleOnGestureListener
     {
--- app/src/main/java/net/sparkly/projectx/views/widgets/CameraView.java
+++ app/src/main/java/net/sparkly/projectx/views/widgets/CameraView.java
@@ -17,6 +17,7 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 
 import net.sparkly.projectx.utils.ClearColor;
+import net.sparkly.projectx.utils.StorageManager;
 
 import org.wysaid.camera.CameraInstance;
 import org.wysaid.common.Common;
@@ -34,6 +35,10 @@ import java.util.List;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
+import static android.hardware.Camera.Parameters.FLASH_MODE_AUTO;
+import static android.hardware.Camera.Parameters.FLASH_MODE_OFF;
+import static android.hardware.Camera.Parameters.FLASH_MODE_ON;
+
 public class CameraView extends GLSurfaceView implements GLSurfaceView.Renderer, SurfaceTexture.OnFrameAvailableListener
 {
     public static final String TAG = CameraView.class.getSimpleName();
@@ -43,6 +48,7 @@ public class CameraView extends GLSurfaceView implements GLSurfaceView.Renderer,
     private SurfaceTexture mSurfaceTexture;
     private CGEFrameRecorder mFrameRecorder;
     private TextureRenderer.Viewport mDrawViewport = new TextureRenderer.Viewport();
+    private StorageManager storageManager;
 
     private int mViewWidth;
     private int mViewHeight;
@@ -328,9 +334,19 @@ public class CameraView extends GLSurfaceView implements GLSurfaceView.Renderer,
         return CameraInstance.getInstance();
     }
 
+    public void setStorageManager(StorageManager storageManager)
+    {
+        this.storageManager = storageManager;
+    }
+
     public interface ReleaseOKCallback
     {
         void releaseOK();
+    }
+
+    public interface TakePictureCallback
+    {
+        void takePictureOK(Bitmap bmp);
     }
 
     public synchronized void release(final ReleaseOKCallback callback)
@@ -366,6 +382,311 @@ public class CameraView extends GLSurfaceView implements GLSurfaceView.Renderer,
     public interface OnCreateCallback
     {
         void createOver(boolean success);
+    }
+
+    // mode value should be:
+    //    Camera.Parameters.FLASH_MODE_AUTO;
+    //    Camera.Parameters.FLASH_MODE_OFF;
+    //    Camera.Parameters.FLASH_MODE_ON;
+    //    Camera.Parameters.FLASH_MODE_RED_EYE
+    //    Camera.Parameters.FLASH_MODE_TORCH 等
+    public synchronized boolean setFlashLightMode(int mode)
+    {
+        String flashRequest = null;
+
+        switch (mode)
+        {
+            case 0:
+                flashRequest = FLASH_MODE_OFF;
+                break;
+            case 1:
+                flashRequest = FLASH_MODE_ON;
+                break;
+            case 2:
+                flashRequest = FLASH_MODE_AUTO;
+                break;
+        }
+
+        if (!getContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH))
+        {
+            Log.e(TAG, "No flash light is supported by current device!");
+            return false;
+        }
+
+        if (!mIsCameraBackForward)
+        {
+            return false;
+        }
+
+        Camera.Parameters parameters = cameraInstance().getParams();
+
+        if (parameters == null)
+            return false;
+
+        try
+        {
+
+            if (!parameters.getSupportedFlashModes().contains(flashRequest))
+            {
+                Log.e(TAG, "Invalid Flash Light Mode!!!");
+                return false;
+            }
+
+            parameters.setFlashMode(flashRequest);
+            cameraInstance().setParams(parameters);
+        } catch (Exception e)
+        {
+            Log.e(TAG, "Switch flash light failed, check if you're using front camera.");
+            return false;
+        }
+
+        return true;
+    }
+
+    public synchronized void takePicture(final CameraSurfaceView.TakePictureCallback photoCallback, Camera.ShutterCallback shutterCallback, final String config, final float intensity, final boolean isFrontMirror)
+    {
+
+        Camera.Parameters params = cameraInstance().getParams();
+
+        if (photoCallback == null || params == null)
+        {
+            Log.e(TAG, "takePicture after release!");
+            if (photoCallback != null)
+            {
+                photoCallback.takePictureOK(null);
+            }
+            return;
+        }
+
+        try
+        {
+            params.setRotation(90);
+            cameraInstance().setParams(params);
+        } catch (Exception e)
+        {
+            Log.e(TAG, "Error when takePicture: " + e.toString());
+            photoCallback.takePictureOK(null);
+            return;
+        }
+
+        cameraInstance().getCameraDevice().takePicture(shutterCallback, null, new Camera.PictureCallback()
+        {
+            @Override
+            public void onPictureTaken(final byte[] data, final Camera camera)
+            {
+                cameraInstance().getCameraDevice().startPreview();
+
+                new Thread(new Runnable()
+                {
+                    @Override
+                    public void run()
+                    {
+                        Camera.Parameters params = camera.getParameters();
+                        Camera.Size sz = params.getPictureSize();
+                        Log.e(TAG, "Width taken: " + sz.width + " height taken:" + sz.height);
+
+                        boolean shouldRotate;
+
+                        Bitmap bmp;
+                        int width, height;
+
+                        if (sz.width != sz.height)
+                        {
+                            //默认数据格式已经设置为 JPEG
+                            bmp = BitmapFactory.decodeByteArray(data, 0, data.length);
+                            width = bmp.getWidth();
+                            height = bmp.getHeight();
+                            shouldRotate = (sz.width > sz.height && width > height) || (sz.width < sz.height && width < height);
+                        } else
+                        {
+                            Log.i(TAG, "Cache image to get exif.");
+
+                            try
+                            {
+                                String tmpFilename = getContext().getExternalCacheDir() + "/picture_cache000.jpg";
+                                FileOutputStream fileout = new FileOutputStream(tmpFilename);
+                                BufferedOutputStream bufferOutStream = new BufferedOutputStream(fileout);
+                                bufferOutStream.write(data);
+                                bufferOutStream.flush();
+                                bufferOutStream.close();
+
+                                ExifInterface exifInterface = new ExifInterface(tmpFilename);
+                                int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+
+                                switch (orientation)
+                                {
+                                    case ExifInterface.ORIENTATION_ROTATE_90:
+                                        shouldRotate = true;
+                                        break;
+                                    default:
+                                        shouldRotate = false;
+                                        break;
+                                }
+
+                                bmp = BitmapFactory.decodeFile(tmpFilename);
+                                width = bmp.getWidth();
+                                height = bmp.getHeight();
+
+                            } catch (IOException e)
+                            {
+                                Log.e(TAG, "Err when saving bitmap...");
+                                e.printStackTrace();
+                                return;
+                            }
+                        }
+
+
+                        if (width > mMaxTextureSize || height > mMaxTextureSize)
+                        {
+                            float scaling = Math.max(width / (float) mMaxTextureSize, height / (float) mMaxTextureSize);
+                            Log.i(TAG, String.format("目标尺寸(%d x %d)超过当前设备OpenGL 能够处理的最大范围(%d x %d)， 现在将图片压缩至合理大小!", width, height, mMaxTextureSize, mMaxTextureSize));
+
+                            bmp = Bitmap.createScaledBitmap(bmp, (int) (width / scaling), (int) (height / scaling), false);
+
+                            width = bmp.getWidth();
+                            height = bmp.getHeight();
+                        }
+
+                        Bitmap bmp2;
+
+                        if (shouldRotate)
+                        {
+                            bmp2 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+                            Canvas canvas = new Canvas(bmp2);
+
+                            if (cameraInstance().getFacing() == Camera.CameraInfo.CAMERA_FACING_BACK)
+                            {
+                                Matrix mat = new Matrix();
+                                int halfLen = Math.min(width, height) / 2;
+                                mat.setRotate(90, halfLen, halfLen);
+                                canvas.drawBitmap(bmp, mat, null);
+                            } else
+                            {
+                                Matrix mat = new Matrix();
+
+                                if (isFrontMirror)
+                                {
+                                    mat.postTranslate(-width / 2, -height / 2);
+                                    mat.postScale(-1.0f, 1.0f);
+                                    mat.postTranslate(width / 2, height / 2);
+                                    int halfLen = Math.min(width, height) / 2;
+                                    mat.postRotate(90, halfLen, halfLen);
+                                } else
+                                {
+                                    int halfLen = Math.max(width, height) / 2;
+                                    mat.postRotate(-90, halfLen, halfLen);
+                                }
+
+                                canvas.drawBitmap(bmp, mat, null);
+                            }
+
+                            bmp.recycle();
+                        } else
+                        {
+                            if (cameraInstance().getFacing() == Camera.CameraInfo.CAMERA_FACING_BACK)
+                            {
+                                bmp2 = bmp;
+                            } else
+                            {
+
+                                bmp2 = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+                                Canvas canvas = new Canvas(bmp2);
+                                Matrix mat = new Matrix();
+                                if (isFrontMirror)
+                                {
+                                    mat.postTranslate(-width / 2, -height / 2);
+                                    mat.postScale(1.0f, -1.0f);
+                                    mat.postTranslate(width / 2, height / 2);
+                                } else
+                                {
+                                    mat.postTranslate(-width / 2, -height / 2);
+                                    mat.postScale(-1.0f, -1.0f);
+                                    mat.postTranslate(width / 2, height / 2);
+                                }
+
+                                canvas.drawBitmap(bmp, mat, null);
+                            }
+
+                        }
+
+                        if (config != null)
+                        {
+                            CGENativeLibrary.filterImage_MultipleEffectsWriteBack(bmp2, config, intensity);
+                        }
+
+                        photoCallback.takePictureOK(bmp2);
+                    }
+                }).start();
+
+            }
+        });
+    }
+
+    public synchronized void takeShot(final CameraSurfaceView.TakePictureCallback callback, final boolean noMask)
+    {
+        assert callback != null : "callback must not be null!";
+
+        if (mFrameRecorder == null)
+        {
+            Log.e(TAG, "Recorder not initialized!");
+            callback.takePictureOK(null);
+            return;
+        }
+
+        queueEvent(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+
+                FrameBufferObject frameBufferObject = new FrameBufferObject();
+                int bufferTexID;
+                IntBuffer buffer;
+                Bitmap bmp;
+
+                if (noMask || !mIsUsingMask)
+                {
+
+                    bufferTexID = Common.genBlankTextureID(mRecordWidth, mRecordHeight);
+                    frameBufferObject.bindTexture(bufferTexID);
+                    GLES20.glViewport(0, 0, mRecordWidth, mRecordHeight);
+                    mFrameRecorder.drawCache();
+                    buffer = IntBuffer.allocate(mRecordWidth * mRecordHeight);
+                    GLES20.glReadPixels(0, 0, mRecordWidth, mRecordHeight, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
+                    bmp = Bitmap.createBitmap(mRecordWidth, mRecordHeight, Bitmap.Config.ARGB_8888);
+                    bmp.copyPixelsFromBuffer(buffer);
+                    Log.i(TAG, String.format("w: %d, h: %d", mRecordWidth, mRecordHeight));
+
+                } else
+                {
+
+                    bufferTexID = Common.genBlankTextureID(mDrawViewport.width, mDrawViewport.height);
+                    frameBufferObject.bindTexture(bufferTexID);
+
+                    int w = Math.min(mDrawViewport.width, mViewWidth);
+                    int h = Math.min(mDrawViewport.height, mViewHeight);
+
+                    mFrameRecorder.setRenderFlipScale(1.0f, 1.0f);
+                    mFrameRecorder.setMaskTextureRatio(mMaskAspectRatio);
+                    mFrameRecorder.render(0, 0, w, h);
+                    mFrameRecorder.setRenderFlipScale(1.0f, -1.0f);
+                    mFrameRecorder.setMaskTextureRatio(mMaskAspectRatio);
+
+                    Log.i(TAG, String.format("w: %d, h: %d", w, h));
+                    buffer = IntBuffer.allocate(w * h);
+                    GLES20.glReadPixels(0, 0, w, h, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, buffer);
+                    bmp = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+                    bmp.copyPixelsFromBuffer(buffer);
+                }
+
+                frameBufferObject.release();
+                GLES20.glDeleteTextures(1, new int[]{bufferTexID}, 0);
+
+                callback.takePictureOK(bmp);
+            }
+        });
+
     }
 
     private void adaptViewPort()
